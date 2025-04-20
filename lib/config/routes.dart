6947325e9071562fe7be4bbfa775376a275d9b83
NalakaDineshSamarasingha <nalakadinesh97@gmail.com>
2025-04-21 00:17:44 +0530

    successfully added google map api

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_frontend/views/map_sample.dart';
 import 'package:mobile_frontend/views/passenger_registration.dart';
 import 'package:mobile_frontend/views/signup_page.dart';
 import '../views/loading_page.dart';
-import '../views/login_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/': (context) => LoadingPage(),
-  '/login': (context) => LoginPage(),
+  '/login': (context) => MapSample(),
   '/signup': (context) => SignupPage(),
   '/passengerRegistration': (context) => PassengerRegistration(),
 
