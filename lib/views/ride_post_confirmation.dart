126742df138317c3853f3ad51d7716201a813e2a
Ayesha Rathnayake <rasanjaleea36@gmail.com>
2025-04-28 06:25:12 +0530

    Background color changed into primary background color

--- lib/views/ride_post_confirmation.dart
+++ lib/views/ride_post_confirmation.dart
@@ -6,7 +6,7 @@ class RidePostConfirmationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0A1029),
+      backgroundColor: const Color(0xFF003366),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
